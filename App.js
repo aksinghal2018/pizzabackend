bde6c4300bb8e65d5ab02c8c8c455ae5d92f1cec
aksinghal2018 <akshay.singhal@krishnacollege.ac.in>
2021-12-06 19:14:24 +0530

    first commit

--- App.js
+++ App.js
@@ -25,3 +25,4 @@ app.use('/',menuRoutes)
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
+  
